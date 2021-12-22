db86e269fa266080795591e9509125ea209556fd
Asma Smail <asma.smail.halouane@gmail.com>
2021-12-22 18:31:30 +0100

    added openzeppelin dependecy
    added openzeppelin dependecy

--- app/src/index.js
+++ app/src/index.js
@@ -60,7 +60,8 @@ window.addEventListener("load", async function() {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
-  } else {
+  } 
+  else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
